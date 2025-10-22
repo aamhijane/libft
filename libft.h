aabb18b62a73861812ce41394af5373734328c49
Ayman Amhijane <ayamhija@e2r2p6.1337.ma>
2025-10-22 16:42:12 +0100

    Adding ft_strlcat, ft_strlpy and ft_toupper functions to the libft.h to define them.

--- libft.h
+++ libft.h
@@ -21,10 +21,13 @@ void  *ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t  ft_strlen(char *str);
+size_t ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isdigit(int n);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
 
 #endif
