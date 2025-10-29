7d6ffc81d77a27da717ffa632f6722f9dab7259f
Ayman Amhijane <ayamhija@e2r5p11.1337.ma>
2025-10-30 00:59:32 +0100

    Creating, and adding a new function (ft_atoi) to our libft.h static lib

--- libft.h
+++ libft.h
@@ -16,6 +16,7 @@
 
 #include <stddef.h>
 
+int ft_atoi(const char *nptr);
 void  ft_bzero(void *s, size_t n);
 void  *ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
