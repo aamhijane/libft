f9e41bc099540cfd2b31f2c2781ea8a7befbe9e8
Ayman Amhijane <ayamhija@e2r2p6.1337.ma>
2025-10-30 14:29:03 +0100

    Creating, and adding a new function (ft_calloc) to our libft.h static lib

--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@
 
 int ft_atoi(const char *nptr);
 void  ft_bzero(void *s, size_t n);
+void *ft_calloc(size_t nmemb, size_t size);
 void  *ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
